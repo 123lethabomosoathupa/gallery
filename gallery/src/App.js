e6d2cc5c666978c8212bcece59eca1d97a85701b
123lethabomosoathupa <99099883+123lethabomosoathupa@users.noreply.github.com>
2022-06-21 08:59:29 +0200

    everything

---
+++ gallery/src/App.js
@@ -0,0 +1,11 @@
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Gallery from "./portfolio-gallery/Gallery";
+function App() {
+ // render(){
+  return (
+    <Gallery />
+  );
+  //}
+}
+export default App;
